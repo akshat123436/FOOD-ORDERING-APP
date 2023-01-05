bfa177af74fa96b9e45768d398c3f57727b2c014
Akshat Kothavade <akshat123436@gmail.com>
2023-01-05 10:39:07 +0530

    added summary

--- first_project_practice/first-project-practice/src/App.js
+++ first_project_practice/first-project-practice/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import React, { Fragment, useState } from "react";
 
 import Header from "./components/Header/Header";
+import MealsSummary from "./components/meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <MealsSummary />
     </Fragment>
   );
 }
