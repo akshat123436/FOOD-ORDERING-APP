833691be8547bd21de99974ecef05805dcd0c861
Akshat Kothavade <akshat123436@gmail.com>
2023-01-06 16:36:52 +0530

    added the bump animation

--- first_project_practice/first-project-practice/src/components/Header/HeaderCartButton.js
+++ first_project_practice/first-project-practice/src/components/Header/HeaderCartButton.js
@@ -1,14 +1,25 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import cardContext from "../../store/card-context";
 const HeaderCartButton = (props) => {
+  const [isButtonHighlighted, setIsButtonHighlighted] = useState(false);
   const cartContext = useContext(cardContext);
   const noOfCartItems = cartContext.items.reduce((currentVal, item) => {
     return currentVal + item.amount;
   }, 0);
+  const classes = `${styles.button} ${isButtonHighlighted ? styles.bump : ""}`;
+  useEffect(() => {
+    setIsButtonHighlighted(true);
+    const timer = setTimeout(() => {
+      setIsButtonHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
   return (
-    <div className={`${styles.button} ${styles.bump}`} onClick={props.onShow}>
+    <div className={classes} onClick={props.onShow}>
       <span>
         <CartIcon className={styles.icon}></CartIcon>
       </span>
