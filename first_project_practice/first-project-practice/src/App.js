ecfb4496f6863446b33ab2b57f7f9fc79c57ae09
Akshat Kothavade <akshat123436@gmail.com>
2023-01-06 05:40:28 +0530

    added meals items list

--- first_project_practice/first-project-practice/src/App.js
+++ first_project_practice/first-project-practice/src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import React, { Fragment, useState } from "react";
 
 import Header from "./components/Header/Header";
-import MealsSummary from "./components/meals/MealsSummary";
+import Meals from "./components/meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <MealsSummary />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- first_project_practice/first-project-practice/src/components/Header/HeaderCartButton.js
+++ first_project_practice/first-project-practice/src/components/Header/HeaderCartButton.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.css";
 const HeaderCartButton = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className={`${styles.button} ${styles.bump}`}>
-      <CartIcon className={styles.icon}></CartIcon>
+      <span>
+        <CartIcon className={styles.icon}></CartIcon>
+      </span>
       <span>Your cart</span>
-      <span className={styles.badge}>0</span>
+      <span className={styles.badge}>{count}</span>
     </div>
   );
 };
